5a774540f585d8fb1c053913eb07f7f3b5b1621a
hugoskorvkiosk <hugohedelin@gmail.com>
2016-09-21 16:48:54 +0200

    component to change sky

---
+++ js/components/set-sky.js
@@ -0,0 +1,13 @@
+
+AFRAME.registerComponent('set-sky', {
+  schema: {default: ''},
+
+  init() {
+    const sky = document.querySelector('a-sky');
+    
+    this.el.addEventListener('click', () => {
+      sky.setAttribute('src', this.data);
+        console.log("hello");
+    });
+  }
+});
