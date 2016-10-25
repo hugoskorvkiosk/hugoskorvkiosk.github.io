7723a8b092cff819f8a80dc1d97fdd739209bb19
hugoskorvkiosk <hugohedelin@gmail.com>
2016-10-25 09:59:35 +0200

    trying out one alternative of the menu item selected popping out

--- js/components/menuselected.js
+++ js/components/menuselected.js
@@ -15,23 +15,29 @@ AFRAME.registerComponent('menuselected', {
         var data = this.data;
         this.el.addEventListener(data.listen, function () {
 
+//
+//            thescene = document.querySelector("#thescene");
+//            thescene.setAttribute('template', 'src', 'scenes/menuscene3.template');
 
-            
-            document.querySelector('#themenutemplatescene').emit('menuitemselect');
-            
+            document.querySelector('#thescene').emit('menuitemselect');
+
+
+
             var themenuplanes = document.querySelectorAll('.menuplane');
-            
-            
+
+
             //TODO - Move the menuitem slected so that it is more visible
             // and move away the menuselections
-            
-            
-            for(var i=0; i < themenuplanes.length; i++){
-                
+
+
+            for (var i = 0; i < themenuplanes.length; i++) {
+
+
+
                 //themenuplanes[i].emit('getminimized');
             }
-            
-            
+
+
 
         });
     }
