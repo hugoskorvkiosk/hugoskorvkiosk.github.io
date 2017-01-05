71433fb0db47e80ba07d0290b9db3f0fd801e95c
hugoskorvkiosk <hugohedelin@gmail.com>
2017-01-05 10:52:08 +0100

    First evaluation test is done. Refining the design. Changed settings with colour and icon. Also changed layout in film menu. Now new,top,downloaded is to the left and the categories are larger and higher up

--- js/components/settingsselection.js
+++ js/components/settingsselection.js
@@ -18,7 +18,9 @@ AFRAME.registerComponent('settingsselection', {
         }
 
     },
-
+    
+  
+          
     update: function () {
         var data = this.data;
         var el = this.el;
@@ -50,8 +52,7 @@ AFRAME.registerComponent('settingsselection', {
             switch (data.setting) {
             case "fade":
                 document.querySelector('#vidbox').setAttribute('unfade-channelmenu', 'active', !data.active);
-                document.querySelector('#settinghidechannels').setAttribute('material', 'color', '#6F6F6F');
-                document.querySelector('#settinghidechannels').setAttribute('settingsselection', 'active', false);
+
                 break;
             case "transparent":
                 document.querySelector('#curvedbg').setAttribute('material', 'opacity', newbgopacity);
@@ -61,8 +62,7 @@ AFRAME.registerComponent('settingsselection', {
                     document.querySelectorAll('.fadewhenvid')[i].setAttribute('visible', 'false');
                     }
                     document.querySelector('#vidbox').setAttribute('unfade-channelmenu', 'active', false);
-                    document.querySelector('#settingfadechannels').setAttribute('material', 'color', '#6F6F6F');
-                    document.querySelector('#settingfadechannels').setAttribute('settingsselection', 'active', false);
+
                 break;
 
             }
@@ -75,14 +75,14 @@ AFRAME.registerComponent('settingsselection', {
         this.el.addEventListener('mouseenter', function () {
 
 
-
             if (data.active) {
                 console.log('active: true ' + data.active);
-                el.setAttribute('material', 'color', '#F19A24');
+                el.setAttribute('material', 'color', '#F12226');
             } else {
-                el.setAttribute('material', 'color', '#F15A24');
+                el.setAttribute('material', 'color', '#22F15A');
             }
-
+            el.setAttribute('material', 'opacity', '0.85');
+            
 
         });
 
@@ -92,18 +92,180 @@ AFRAME.registerComponent('settingsselection', {
 
             if (data.active) {
                 console.log(data.active);
-                el.setAttribute('material', 'color', '#A43D18');
+                el.setAttribute('material', 'opacity', '0.7');
+                el.setAttribute('material', 'color', '#22F15A');
             } else {
-                el.setAttribute('material', 'color', '#6F6F6F');
+                
+                el.setAttribute('material', 'color', '#ffffff');
+                el.setAttribute('material', 'opacity', '0.2');
             }
 
 
+        });
+        
+          this.el.addEventListener('click', function () {
 
+
+
+            if (!data.active) {
+                console.log(data.active);
+                el.setAttribute('material', 'opacity', '0.7');
+                el.setAttribute('material', 'color', '#22F15A');
+            } else {
+                
+                el.setAttribute('material', 'color', '#ffffff');
+                el.setAttribute('material', 'opacity', '0.2');
+            }
 
 
         });
+        
 
     },
 
 
 });
+
+//
+//
+//// Component to change to color on click.
+//AFRAME.registerComponent('settingsselection', {
+//
+//
+//    schema: {
+//        listen: {
+//            default: 'click'
+//        },
+//        target: {
+//            type: 'selector'
+//        },
+//
+//        active: {
+//            type: 'boolean'
+//        },
+//        setting: {
+//            default: 'fade'
+//        },
+//        onoff: {
+//            default: 'on'   
+//        }
+//
+//    },
+//
+//    update: function () {
+//        var data = this.data;
+//        var el = this.el;
+//        
+//
+//        this.el.addEventListener(data.listen, function () {
+//
+//            var newbgopacity;
+//           
+//            var fadewhenvidobjects = document.querySelectorAll('.fadewhenvid');
+//           
+//            var onoroff =  el.getAttribute('onoff');
+//            
+//            var onbutton = document.querySelector('#' + data.setting + 'settingOn');
+//            var offbutton = document.querySelector('#' + data.setting + 'settingOff');
+//            
+//            if(data.active){
+//            return;
+//            }
+//            else{
+//            
+//                if(onoroff == 'off'){
+//                
+//                    offbutton.setAttribute('settingselection', 'active', true);
+//                    offbutton.setAttribute('material','opacity', '1');
+//                    onbutton.setAttribute('settingselection', 'active', false);
+//                    onbutton.setAttribute('material','opacity', '0.5');
+//                
+//                }
+//                else{
+//                
+//                    onbutton.setAttribute('settingselection', 'active', true);
+//                    onbutton.setAttribute('material','opacity', '1');
+//                    offbutton.setAttribute('settingselection', 'active', false);
+//                    offbutton.setAttribute('material','opacity', '0.5');
+//                
+//                }
+//                
+//           
+//            
+//            }
+//            
+//            /*
+//            
+//            om !active 
+//             om onoff == off
+//                off set active
+//                off set opacity 1
+//                on set !active
+//                on set opacity 0.5
+//            
+//            */
+//            
+//            if (!data.active) {
+//
+//                console.log("deactivating");
+//
+//                newbgopacity = '1';
+//          
+//
+//            } else {
+//
+//                console.log("activating");
+//
+//                newbgopacity = '0.55';
+//       
+//            }
+//
+//            
+//
+//            switch (data.setting) {
+//            case "fade":
+//                document.querySelector('#vidbox').setAttribute('unfade-channelmenu', 'active', !data.active);
+//
+//                break;
+//            case "transparent":
+//                document.querySelector('#curvedbg').setAttribute('material', 'opacity', newbgopacity);
+//                break;
+//            case "hide":
+//                for (var i = 0; i < fadewhenvidobjects.length; i++) {
+//                    document.querySelectorAll('.fadewhenvid')[i].setAttribute('visible', 'false');
+//                    }
+//                    document.querySelector('#vidbox').setAttribute('unfade-channelmenu', 'active', false);
+//
+//                break;
+//
+//            }
+//
+//
+//        });
+//
+//
+//        this.el.addEventListener('mouseenter', function () {
+//
+//            if (!data.active) {
+//                el.setAttribute('material', 'opacity', '1');
+//            } 
+//
+//
+//        });
+//
+//        this.el.addEventListener('mouseleave', function () {
+//
+//
+//            if (!data.active) {
+//                console.log(data.active);
+//                 el.setAttribute('material', 'opacity', '0.5');
+//            }
+//
+//
+//
+//        });
+//
+//    },
+//
+//
+//});
